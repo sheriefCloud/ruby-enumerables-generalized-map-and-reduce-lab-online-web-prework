98e2acdd2c10372157205ef56f3ab4fc0ac0c08c
Sherief Farid <sherief@farid.cloud>
2020-01-04 19:36:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new=[]
+  counter = 0
+  while counter < array.length
+    new.push(yield(array[counter]))
+    counter += 1
+  end
+  new
+end
+
+def reduce(array, starting_point=nil) # will not expect a starting_point unless give explecetly
+  if starting_point
+    sum = starting_point
+    counter = 0
+  else
+    sum = array[0]
+    counter = 1
+  end
+  while counter < array.size
+    sum = yield(sum, array[counter])
+    counter += 1
+  end
+  sum
+end
